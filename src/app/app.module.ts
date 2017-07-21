a525e2f1f597967c3728a1a354cb053d26fa231d
Wilson Espinosa Bolivar <wilson.espinosa@yuxiglobal.com>
2017-07-21 16:08:56 -0500

    first commit_

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { HeroSearchComponent } from './heroes/hero-search/hero-search.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+ 
+import { AppRoutingModule } from './app-routing.module';
+ 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+ 
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { HeroService } from './services/hero.service';
+
+@NgModule({
+  imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
+      AppRoutingModule],
+  declarations: [AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mocks/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Abante' },
+  { id: 12, name: 'Belerofonte' },
+  { id: 13, name: 'Clitoneo' },
+  { id: 14, name: 'Deífobo' },
+  { id: 15, name: 'Eneas' },
+  { id: 16, name: 'Ganimedes' },
+  { id: 17, name: 'Héctor' },
+  { id: 18, name: 'Layo' },
+  { id: 19, name: 'Menelao' },
+  { id: 20, name: 'Teseo' }
+];
